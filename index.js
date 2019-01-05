881f1ddbd690ebf28da394bffe65eac00b75c522
brambow <bates@cartolab.com>
2019-01-05 16:49:14 -0500

    do some more to add the projection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const d3 = require('d3-geo')
-Object.assign(d3, require('d3-geo-projection'))
+const d3 = require('d3-geo');
+Object.assign(d3, require('d3-geo-projection'));
 
-module.exports = reproject
+module.exports = reproject;
 
 /**
  * Reprojects the given geometry coordinate array _in place_, with
@@ -15,71 +15,70 @@ module.exports = reproject
  * @param {Object} [options.projections] A map of named projections to use.  If provided, then string values of `options.forward` or `options.reverse` will be used as keys to look up the projection function in `options.projections`.  For an extensive list provided by d3-geo-projection, use `require('dirty-reprojectors/projections')`.
  * @param {Object} geometry A GeoJSON geometry object
  */
-function reproject (options, geometry) {
-  let streams = []
+function reproject(options, geometry) {
+  let streams = [];
 
   if (options.forward) {
-    let proj = options.forward
+    let proj = options.forward;
     if (typeof proj === 'string') {
-      proj = options.projections[proj]
+      proj = options.projections[proj];
     }
-    streams.push(proj.stream, flipY())
+    streams.push(proj.stream, flipY());
   }
 
   if (options.reverse) {
-    let proj = options.reverse
+    let proj = options.reverse;
     if (typeof proj === 'string') {
-      proj = options.projections[proj]
+      proj = options.projections[proj];
     }
-    streams.push(reverse(proj), flipY())
+    streams.push(reverse(proj), flipY());
   }
 
-  streams.reverse()
+  streams.reverse();
 
   const projection = {
-    stream: function (output) {
-      return streams.reduce((combined, s) => s(combined), output)
+    stream: function(output) {
+      return streams.reduce((combined, s) => s(combined), output);
     }
-  }
+  };
 
-  return d3.geoProject(geometry, projection)
+  return d3.geoProject(geometry, projection);
 }
 
-function reverse (projection) {
-  let prev = []
+function reverse(projection) {
+  let prev = [];
   return d3.geoTransform({
-    point: function (x, y) {
+    point: function(x, y) {
       // prevent wrapping due to precision issues
-      x = clamp(x, -20037508.34278924, 20037508.34278924)
-      var reversed = projection.invert([x, y])
+      x = clamp(x, -20037508.34278924, 20037508.34278924);
+      var reversed = projection.invert([x, y]);
 
       // drop points that fail to project or are identical to the previous
       // point
       if (!reversed || (reversed[0] === prev[0] && reversed[1] === prev[1])) {
-        return
+        return;
       }
 
-      prev = reversed
-      this.stream.point(reversed[0], reversed[1])
+      prev = reversed;
+      this.stream.point(reversed[0], reversed[1]);
     }
-  }).stream
+  }).stream;
 }
 
-function flipY () {
+function flipY() {
   return d3.geoTransform({
-    point: function (x, y) {
-      this.stream.point(x, -y)
+    point: function(x, y) {
+      this.stream.point(x, -y);
     }
-  }).stream
+  }).stream;
 }
 
-function clamp (value, min, max) {
+function clamp(value, min, max) {
   if (value > max) {
-    return max
+    return max;
   } else if (value < min) {
-    return min
+    return min;
   } else {
-    return value
+    return value;
   }
 }
-
--- projections/index.js
+++ projections/index.js
@@ -1,9 +1,10 @@
-let d3 = require('d3-geo')
-Object.assign(d3, require('d3-geo-projection'))
+let d3 = require('d3-geo');
+Object.assign(d3, require('d3-geo-projection'));
 
-let R = 6378137.0 // radius of Earth in meters
+let R = 6378137.0; // radius of Earth in meters
 
-const projections = module.exports
+const projections = module.exports;
+const albersUsaTerritories = require('./albers-usa-w-territories');
 
 const supported = [
   'albers',
@@ -107,15 +108,21 @@ const supported = [
   'wagner7',
   'wiechel',
   'winkel3'
-]
+];
 
 for (const d3name in d3) {
-  const match = /(?:geo)([A-Z])(.*)/.exec(d3name)
-  if (!match) { continue }
-  const name = match[1].toLowerCase() + match[2]
-  if (supported.indexOf(name) < 0) {
-    continue
+  const match = /(?:geo)([A-Z])(.*)/.exec(d3name);
+  if (!match) {
+    continue;
   }
-  projections[name] = d3[d3name]().translate([0, 0]).scale(R)
+  const name = match[1].toLowerCase() + match[2];
+  if (supported.indexOf(name) < 0) {
+    continue;
+  }
+  projections[name] = d3[d3name]()
+    .translate([0, 0])
+    .scale(R);
 }
 
+//add albersUsaTerritories
+projections.albersUsaTerritories = albersUsaTerritories;
